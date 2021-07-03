1dc6c30e8ad4983a1cf387b0bef2bca81a975289
daan03er <daandendikken1510@gmail.com>
2021-07-03 17:50:57 +0200

    updated code.ino with defines for pins and foundation functions

--- Code/Code.ino
+++ Code/Code.ino
@@ -4,15 +4,33 @@
 #include "SoftwareSerial.h"
 #include "DFRobotDFPlayerMini.h"
 
+//extra pin 
 #define busy 7
+#define interruptPin 2
 
-static const uint8_t PIN_MP3_TX = 2; // Connects to module's RX
-static const uint8_t PIN_MP3_RX = 3; // Connects to module's TX
-				     //use pins 2 and 3 to communicate with dfminplayer 
+//pins for buttons
+#define pause_play_button 13
+#define previous_button 12
+#define next_button 11
+#define stop_button 10
+#define shuffle_button 9
+
+//pins for display
+#define display_data1 A0
+#define display_data2 A1
+#define display_data3 A2
+#define display_data4 A3
+#define display_enable A4
+#define display_RS A5
+
+//use pins 4 and 5 to communicate with dfminiplayer
+static const uint8_t PIN_MP3_TX = 4; // Connects to module's RX
+static const uint8_t PIN_MP3_RX = 5; // Connects to module's TX
 
 SoftwareSerial softwareSerial(PIN_MP3_RX, PIN_MP3_TX);
 DFRobotDFPlayerMini player;
 
+volatile byte state = LOW;
 int teller = 1; //teller used for playing next song
 
 void setup_serial(){
@@ -34,6 +52,11 @@ void setup_pins(){
   pinMode(busy, INPUT);
 }
 
+void setup_interrupt(){
+  pinMode(interrupt_pin, INPUT_PULLUP); 
+  attachInterrupt(digitalPinToInterrupt(interruptPin), read, CHANGE); 
+}
+
 void setup() {
   setup_pins(); 
   setup_serial(); 
@@ -49,4 +72,21 @@ void loop() {
   }
   else{
   
+}
+
+//reading the 5 individual buttons and returning the high or low boolean of every button
+//since only 2 pins can be used for interrupt, all the buttons are connected to pin 2 for the interrupt and a separate pin for the data
+int read_button(){
+  digitalRead(
+}
+
+//function for writing or commanding to the display, with 2 parameters
+//look at MP3 code blok 3 for inspiration  
+void write_display(boolean RW, int info){
+
+}
+
+//used for interrupt function, changing the state 
+void read(){
+  state = !state; 
 }
